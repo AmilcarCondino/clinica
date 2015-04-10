b0c4774a9976de8563e6feb2c18528dc2d7b9b22
Amilcar Condino <amilcar.condino@gmail.com>
2015-04-10 11:28:54 -0300

    no oser login

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+// composer require laracasts/testdummy
+use Laracasts\TestDummy\Factory as TestDummy;
+
+class UsersTableSeeder extends Seeder {
+
+    public function run()
+    {
+        Eloquent::unguard();
+
+        User::create([
+
+            'username' => 'admin',
+            'password' => Hash::make('456'),
+            'email' => 'admin@gmail.com',
+            'rol' => 'admin',
+            'first_name' => 'Jhon Doe',
+            'country' => 'Argentina',
+            'city' => 'CABA',
+            'gender' => 'Masculino',
+        ]);
+    }
+
+}
